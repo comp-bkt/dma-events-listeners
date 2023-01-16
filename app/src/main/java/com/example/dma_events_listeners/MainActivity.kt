82c5176dda751ef5045e824ec6bf75197fcd64b3
pat <p@p>
2023-01-16 15:44:01 +0000

    added an object to hold the OnClickListener

--- app/src/main/java/com/example/dma_events_listeners/MainActivity.kt
+++ app/src/main/java/com/example/dma_events_listeners/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.dma_events_listeners
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    private val mButtonListener = View.OnClickListener {
+        /*use 'it' for the View object*/
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
