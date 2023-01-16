6b73ae3a38619a1a89b5a1cd900a1dd41a7d5b53
pat <p@p>
2023-01-16 14:19:00 +0000

    added textView objects to match the TextViews defined on xml layout

--- app/src/main/java/com/example/dma_events_listeners/MainActivity.kt
+++ app/src/main/java/com/example/dma_events_listeners/MainActivity.kt
@@ -3,11 +3,21 @@ package com.example.dma_events_listeners
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView1:TextView
+    private lateinit var textView2:TextView
+    private lateinit var textView3:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView1 = findViewById(R.id.textView1)
+        textView2 = findViewById(R.id.textView2)
+        textView3 = findViewById(R.id.textView3)
     }
 
     fun button1Clicked(view: View) {
