ff332f6574afccec00e28ff1f9302c27800c83f1
pat <p@p>
2023-01-16 11:27:38 +0000

    start point - default empty activity project

---
+++ app/src/main/java/com/example/dma_events_listeners/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dma_events_listeners
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
