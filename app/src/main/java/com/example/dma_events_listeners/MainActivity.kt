3c753a578c8f4186718ae1c1958fa0f6eade74e3
pat <p@p>
2023-01-16 14:16:48 +0000

    added button action as xml attribute and created associated method in MainActivity

--- app/src/main/java/com/example/dma_events_listeners/MainActivity.kt
+++ app/src/main/java/com/example/dma_events_listeners/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.dma_events_listeners
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun button1Clicked(view: View) {
+
+    }
+    fun button2Clicked(view: View) {
+
+    }
+    fun button3Clicked(view: View) {
+
     }
 }
