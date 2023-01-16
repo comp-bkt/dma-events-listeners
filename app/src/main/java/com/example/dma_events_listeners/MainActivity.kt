227974f141a6be4dca943c7867f08bce9d05b12d
pat <p@p>
2023-01-16 14:26:21 +0000

    added a Handler to run a 3s timer to remove text from view

--- app/src/main/java/com/example/dma_events_listeners/MainActivity.kt
+++ app/src/main/java/com/example/dma_events_listeners/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.dma_events_listeners
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.TextView
 
@@ -22,11 +24,20 @@ class MainActivity : AppCompatActivity() {
 
     fun button1Clicked(view: View) {
         textView1.text = "Button 1 Clicked"
+        removeTextFromView(textView1)
     }
     fun button2Clicked(view: View) {
         textView2.text = "Button 2 Clicked"
+        removeTextFromView(textView2)
     }
     fun button3Clicked(view: View) {
         textView3.text = "Button 3 Clicked"
+        removeTextFromView(textView3)
+    }
+
+    private fun removeTextFromView(tv:TextView) {
+        Handler(Looper.getMainLooper()).postDelayed({
+            tv.text=""
+        }, 3000)
     }
 }
