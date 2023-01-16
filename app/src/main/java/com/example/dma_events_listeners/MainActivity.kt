50be3c0fbac3ddf223f27f59c890095ee9e69836
pat <p@p>
2023-01-16 15:09:54 +0000

    added a handler to remove text from textview after 3 seconds

--- app/src/main/java/com/example/dma_events_listeners/MainActivity.kt
+++ app/src/main/java/com/example/dma_events_listeners/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.dma_events_listeners
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -26,19 +28,28 @@ class MainActivity : AppCompatActivity() {
         button1.setOnClickListener{
             val tv:TextView = findViewById(R.id.textView1)
             tv.text = "Button 1 Clicked"
+            removeTextFromView(tv)
         }
 
         button2.setOnClickListener(object: View.OnClickListener {
             override fun onClick(view: View) {
                 val tv:TextView = findViewById(R.id.textView2)
                 tv.text = "Button 2 Clicked"
+                removeTextFromView(tv)
             }
         })
 
         button3.setOnClickListener{
             val tv:TextView = findViewById(R.id.textView3)
             tv.text = "Button 3 Clicked"
+            removeTextFromView(tv)
         }
 
+    }
+
+    private fun removeTextFromView(tv:TextView) {
+        Handler(Looper.getMainLooper()).postDelayed({
+            tv.text=""
+        }, 3000)
     }
 }
